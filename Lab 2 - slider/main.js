b112d93b3add8e1eee0d482a0897622c9aa671f7
luznaszelkach <jakubbrandys99@gmail.com>
2023-01-08 00:08:21 +0100

    Lab2

---
+++ Lab 2 - slider/main.js
@@ -0,0 +1,34 @@
+let slideIndex = 1;
+
+let moveRightInterval = window.setInterval((function(){plusSlides(1)}), 5000)
+
+function plusSlides(n) 
+{
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n)
+{
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) 
+{
+  clearInterval(moveRightInterval);
+  moveRightInterval = window.setInterval((function(){plusSlides(1)}), 5000)
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) 
+  {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) 
+  {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
