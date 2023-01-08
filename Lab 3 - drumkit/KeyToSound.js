37a516b7b53fd4cfe3a3cdc201b2134cbe9801f9
luznaszelkach <jakubbrandys99@gmail.com>
2023-01-08 14:05:05 +0100

    Lab3

---
+++ Lab 3 - drumkit/KeyToSound.js
@@ -0,0 +1,11 @@
+export const KeyToSound = {
+    '1': 'boom',
+    '2': 'clap',
+    '3': 'hihat',
+    '4': 'kick',
+    '5': 'openhat',
+    '6': 'ride',
+    '7': 'snare',
+    '8': 'tink',
+    '9': 'tom'
+}
